288c5ee5e0ba4e9a44737c70f311d6a75643c19d
Mark Anderson <andersm3@tcd.ie>
2022-09-01 22:20:10 +0100

    Add tests

--- src/test/test_filters.py
+++ src/test/test_filters.py
@@ -1,4 +1,5 @@
 import os
+from tkinter import W
 import numpy as np
 
 #torchaudio
@@ -17,7 +18,23 @@ class GammatoneTest(unittest.TestCase):
         self.audio, self.fs = torchaudio.load("test/resources/arctic_b0041.wav")
         print("ok?!")
 
-    def default_test(self):
+    def test_gabor(self):
+        frontend = GroupedFrontend(
+                n_filters=40,
+                min_freq=80.,
+                max_freq=8000.,
+                sample_rate=16000,
+                compression=None,
+                init_filter="mel",
+                filter_type="gabor"
+            )
+
+        x = frontend(self.audio)
+        x = x.detach().numpy()
+        # x = x.squeeze().T
+        # x.astype(np.float32).tofile(d[2])
+
+    def test_gammatone(self):
         frontend = GroupedFrontend(
                 n_filters=40,
                 min_freq=80.,
@@ -32,3 +49,7 @@ class GammatoneTest(unittest.TestCase):
         x = x.detach().numpy()
         # x = x.squeeze().T
         # x.astype(np.float32).tofile(d[2])
+
+
+if __name__ == '__main__':
+    unittest.main()
